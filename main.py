ef174b6420972cb544ea51ca4a093fc86f3c0a83
Anton <tohsa01@gmail.com>
2026-03-11 21:14:12 +0300

    refactor methods, add pictures

--- main.py
+++ main.py
@@ -1,67 +1,82 @@
 import random
-import pictures 
-alf= "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзхийклмнопрстуфхцчшщъыьэюя"
+from pictures import get_picture
+
+ALPHABET= "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзхийклмнопрстуфхцчшщъыьэюя"
+
 def init_dictionary():
      with open("words.txt", 'r', encoding='utf-8') as file:
         text = file.read()
         words = text.split()
-        return words      
-words = init_dictionary() 
-def choose_hard(hard_level):
-    match hard_level:
-        case "н":
-            return 11
-        case "р":
-            return 8
-        case _:
-            return 5
-def get_char():
-    
-    ch = input("Введите букву русского алфавита ")  
-    if len(ch) == 1 and ch in alf:
-        return ch
+        return words   
+        
+
+
+
+
+def get_char_from_user():
+    char = input("Введите букву русского алфавита ")  
+    if len(char) == 1 and char in ALPHABET:
+        return char.lower()
     else:
         while True:
-            ch = input("Неверный ввод! Введите русскую букву ")
-            if ch in alf:
-                return ch
+            char = input("Неверный ввод! Введите русскую букву ")
+            if len(char) ==1 and char in ALPHABET:
+                return char.lower()
 
-def is_game_over(WORD, word, mistakes):
-    if ('*' not in word) or mistakes == 0:
+def is_game_over(word_mask, mistakes):
+    if ('*' not in word_mask or mistakes == 0):
         return True
     return False
-def open_chars(WORD, word, ch):
+
+def open_chars(WORD, word_mask, char):
     for i in range(len(WORD)):
-        if ch==WORD[i]:
-            word[i] = WORD[i] 
-    return word
+        if char==WORD[i]:
+            word_mask[i] = WORD[i] 
+
+def say_is_char_in_mask(WORD, word_mask, char):
+    if char in word_mask:
+        print("Есть такая буква!")
+    else:
+        print("Нет такой буквы!")
+
+def show_start_menu():
+    print("Чтобы начать игру, нажмите любую букву, чтобы выйти, нажмите в ")
+    inp = input()
+    if inp =="в":
+        print("Программа завершена")
+        exit()
 
 
-def start_game():
-    inp = input("Чтобы нажать игру, нажмите любую букву, чтобы выйти, нажмите в ")
-    if inp=="в":
-        exit() 
-    print("Выберите уровень сложности")
-    hard_level = input("Низкий (11 попыток): нажмите н, средний(8) - р, высокий(5) - любая другая буква")
-    mistakes = choose_hard(hard_level) 
-    pict = 0 
+def start_game(words: list):
+    show_start_menu()
+    
     WORD = random.choice(words) 
-    word = list(len(WORD) * '*')
+    word_mask = list(len(WORD) * '*')
     chars_history = set() 
-    while(not is_game_over(WORD, word, mistakes)):
-        print(f"У вас есть {mistakes} попыток чтобы отгадать слово {''.join(word)}")
-        ch = get_char() 
-        if ch not in chars_history and ch in alf:
-            chars_history.add(ch) 
-            pict+=1
-            mistakes-=1
-        word = open_chars(WORD, word, ch) 
-        print(pictures.pictures[pict])
+    mistakes = 7
+
+    while(not is_game_over(word_mask, mistakes)):
+        print(f"У вас есть {mistakes} попыток чтобы отгадать слово {''.join(word_mask)}")
+        char = get_char_from_user() 
+        if char in chars_history:
+                print("Вы уже вводили такую букву. Введите другую")
+        elif char not in chars_history:
+            chars_history.add(char) 
+            if char not in WORD: 
+                mistakes-=1
+            open_chars(WORD, word_mask, char)
+            say_is_char_in_mask(WORD, word_mask, char)
+        print(get_picture(mistakes))
+
     if mistakes==0:
         print(f"Вы проиграли! Было загадано слово {WORD}") 
     if mistakes>0:
-        print(f"Вы выйграли! Было загадано слово {WORD}")
+        print(f"Вы выиграли! Было загадано слово {WORD}")
 
-while True:
-    start_game()
+if __name__ == "__main__":
+    words = init_dictionary()
+    while True:
+        start_game(words)
+    
+
 
